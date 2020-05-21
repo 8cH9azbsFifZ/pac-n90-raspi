e86a032b6527d3d43e398348c9d08c23df2ced7b
Gerolf Ziegenhain <g@ziegenhain.com>
2020-05-21 21:26:01 +0200

    copied

---
+++ ir-slinger/test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "irslinger.h"
+
+#define on 	"00010010000100010001010100110010"
+#define off 	"00010010000100011000010000110010"
+
+int main(int argc, char *argv[])
+{
+	uint32_t outPin = 24;            // The Broadcom pin number the signal will be sent on
+	int frequency = 38000;           // The frequency of the IR signal in Hz
+	double dutyCycle = 0.5;          // The duty cycle of the IR signal. 0.5 means for every cycle,
+	                                 // the LED will turn on for half the cycle time, and off the other half
+	int leadingPulseDuration = 9000; //9102; // The duration of the beginning pulse in microseconds
+	int leadingGapDuration = 4500;//4450;   // The duration of the gap in microseconds after the leading pulse
+	int onePulse = 560;              // The duration of a pulse in microseconds when sending a logical 1
+	int zeroPulse = 560;             // The duration of a pulse in microseconds when sending a logical 0
+	int oneGap = 1687; //1600;               // The duration of the gap in microseconds when sending a logical 1
+	int zeroGap = 560;               // The duration of the gap in microseconds when sending a logical 0
+	int sendTrailingPulse = 1;       // 1 = Send a trailing pulse with duration equal to "onePulse"
+	                                 // 0 = Don't send a trailing pulse
+
+
+	int result = irSling(
+		outPin,
+		frequency,
+		dutyCycle,
+		leadingPulseDuration,
+		leadingGapDuration,
+		onePulse,
+		zeroPulse,
+		oneGap,
+		zeroGap,
+		sendTrailingPulse,
+		on);
+//		"00010010000100011000010000110010");
+//		  "00010010001010001000000100010000");
+		//"01000001101101100101100010100111");
+	
+	return result;
+}
