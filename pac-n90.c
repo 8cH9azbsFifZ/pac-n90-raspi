8e567c86e27b3149beffccd84cbb26a9b510d9f6
Gerolf Ziegenhain <g@ziegenhain.com>
2020-05-21 22:18:38 +0200

    merged

--- pac-n90.c
+++ pac-n90.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include "ir-slinger/irslinger.h" // FIXME: dir
+
+
+#define on      "00010010000100010001010100110010" // FIXME: remove
+#define off     "00010010000100011000010000110010"
+
 typedef struct {
   bool on;
   bool timer;
@@ -162,6 +168,21 @@ char* returnBits(size_t const size, void const * const ptr)
 
 int main (void)
 {
+          uint32_t outPin = 24;            // The Broadcom pin number the signal will be sent on
+        int frequency = 38000;           // The frequency of the IR signal in Hz
+        double dutyCycle = 0.5;          // The duty cycle of the IR signal. 0.5 means for every cycle,
+                                         // the LED will turn on for half the cycle time, and off the other half
+        int leadingPulseDuration = 9000; //9102; // The duration of the beginning pulse in microseconds
+        int leadingGapDuration = 4500;//4450;   // The duration of the gap in microseconds after the leading pulse
+        int onePulse = 560;              // The duration of a pulse in microseconds when sending a logical 1
+        int zeroPulse = 560;             // The duration of a pulse in microseconds when sending a logical 0
+        int oneGap = 1687; //1600;               // The duration of the gap in microseconds when sending a logical 1
+        int zeroGap = 560;               // The duration of the gap in microseconds when sending a logical 0
+        int sendTrailingPulse = 1;       // 1 = Send a trailing pulse with duration equal to "onePulse"
+                                         // 0 = Don't send a trailing pulse
+
+
+ 
   dl_aircon_msg_t msg;
 
   //Default settings
@@ -176,4 +197,21 @@ int main (void)
   unsigned long data = dl_assemble_msg(&msg);
   char *result = returnBits(sizeof(data), &data);
   printf ("%s",result);
+
+        int result1 = irSling(
+                outPin,
+                frequency,
+                dutyCycle,
+                leadingPulseDuration,
+                leadingGapDuration,
+                onePulse,
+                zeroPulse,
+                oneGap,
+                zeroGap,
+                sendTrailingPulse,
+                result);
+//              "00010010000100011000010000110010");
+//                "00010010001010001000000100010000");
+                //"01000001101101100101100010100111");
+
 }
