e7857d97fc102643b8d1d1277c5a1f711c1bbdcd
Gerolf Ziegenhain <g@ziegenhain.com>
2020-05-22 20:59:40 +0200

    wip

--- pac-n90.c
+++ pac-n90.c
@@ -149,27 +149,6 @@ unsigned char bit_reverse( unsigned char x )
 }
 
 
-// Refs: 
-// https://stackoverflow.com/questions/111928/is-there-a-printf-converter-to-print-in-binary-format
-// https://stackoverflow.com/questions/41384262/convert-string-to-binary-in-c
-void printBits(size_t const size, void const * const ptr, char *result)
-{
-    unsigned char *b = (unsigned char*) ptr;
-    unsigned char byte;
-    int i, j;
-
-    for (i=size-1;i>=0;i--)
-    {
-        for (j=7;j>=0;j--)
-        {
-            byte = (b[i] >> j) & 1;
-            printf("%u", byte);
-        }
-    }
-    puts("");
-}
-
-
 char* returnBits(size_t const size, void const * const ptr)
 {
     unsigned char *b = (unsigned char*) ptr;
