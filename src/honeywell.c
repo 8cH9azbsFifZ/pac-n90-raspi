d38d9a9538af395d5216e4cd1b95779f49c22a20
Gerolf Ziegenhain <g@ziegenhain.com>
2020-05-24 02:11:18 +0200

    wip

--- src/honeywell.c
+++ src/honeywell.c
@@ -46,9 +46,9 @@
 #define WAVE_WOOD                     8
 #define WAVE_MOON                     2
 #define WAVE_NONE                     1
-#define MODE_AIRCONDITIONING_NAME     "Wood"
-#define MODE_DEHUMIDIFY_NAME          "Moon"
-#define MODE_BLOW_NAME                "None"
+#define WAVE_WOOD_NAME                "Wood"
+#define WAVE_MOON_NAME                "Moon"
+#define WAVE_NONE_NAME                "None"
 
 #define FAN_LOW                       4
 #define FAN_MID                       2 
@@ -97,127 +97,7 @@ int send_power(void)
 	double dutyCycle = 0.5;         // The duty cycle of the IR signal. 0.5 means for every cycle,
 	                                // the LED will turn on for half the cycle time, and off the other half
 
-	int codes[] = {
-1258 , 
-383 , 
-1293 , 
-390 , 
-454 , 
-1226 , 
-1292 , 
-390 , 
-1293 , 
-396 , 
-460 , 
-1234 , 
-459 , 
-1238 , 
-456 , 
-1237 , 
-457 , 
-1236 , 
-461 , 
-1234 , 
-456 , 
-1238 , 
-1293 , 
-7173 , 
-1293 , 
-387 , 
-1288 , 
-391 , 
-457 , 
-1228 , 
-1294 , 
-389 , 
-1286 , 
-397 , 
-459 , 
-1236 , 
-459 , 
-1234 , 
-458 , 
-1238 , 
-456 , 
-1237 , 
-459 , 
-1235 , 
-460 , 
-1234 , 
-1293 , 
-7213 , 
-1294 , 
-384 , 
-1293 , 
-383 , 
-461 , 
-1227 , 
-1292 , 
-396 , 
-1283 , 
-400 , 
-458 , 
-1238 , 
-456 , 
-1235 , 
-458 , 
-1238 , 
-459 , 
-1236 , 
-458 , 
-1234 , 
-463 , 
-1229 , 
-1293 , 
-7189 , 
-1292 , 
-387 , 
-1288 , 
-392 , 
-457 , 
-1227 , 
-1292 , 
-388 , 
-1289 , 
-399 , 
-457 , 
-1236 , 
-457 , 
-1240 , 
-455 , 
-1243 , 
-453 , 
-1240 , 
-454 , 
-1242 , 
-451 , 
-1235 , 
-1295 , 
-7188 , 
-1291 , 
-389 , 
-1288 , 
-389 , 
-459 , 
-1227 , 
-1290 , 
-392 , 
-1286 , 
-399 , 
-458 , 
-1235 , 
-458 , 
-1243 , 
-452 , 
-1241 , 
-453 , 
-1242 , 
-451 , 
-1243 , 
-458 , 
-1230 , 
-1298 
-};
+	int codes[] = { 1258 , 383 , 1293 , 390 , 454 , 1226 , 1292 , 390 , 1293 , 396 , 460 , 1234 , 459 , 1238 , 456 , 1237 , 457 , 1236 , 461 , 1234 , 456 , 1238 , 1293 , 7173 , 1293 , 387 , 1288 , 391 , 457 , 1228 , 1294 , 389 , 1286 , 397 , 459 , 1236 , 459 , 1234 , 458 , 1238 , 456 , 1237 , 459 , 1235 , 460 , 1234 , 1293 , 7213 , 1294 , 384 , 1293 , 383 , 461 , 1227 , 1292 , 396 , 1283 , 400 , 458 , 1238 , 456 , 1235 , 458 , 1238 , 459 , 1236 , 458 , 1234 , 463 , 1229 , 1293 , 7189 , 1292 , 387 , 1288 , 392 , 457 , 1227 , 1292 , 388 , 1289 , 399 , 457 , 1236 , 457 , 1240 , 455 , 1243 , 453 , 1240 , 454 , 1242 , 451 , 1235 , 1295 , 7188 , 1291 , 389 , 1288 , 389 , 459 , 1227 , 1290 , 392 , 1286 , 399 , 458 , 1235 , 458 , 1243 , 452 , 1241 , 453 , 1242 , 451 , 1243 , 458 , 1230 , 1298 };
 
 	int result = irSlingRaw(
 		outPin,
@@ -239,126 +119,7 @@ int send_fan (void)
 	double dutyCycle = 0.5;         // The duty cycle of the IR signal. 0.5 means for every cycle,
 	                                // the LED will turn on for half the cycle time, and off the other half
 
-	int codes[] = {
-1257 , 
-385 , 
-1296 , 
-384 , 
-457 , 
-1230 , 
-1290 , 
-386 , 
-1295 , 
-394 , 
-462 , 
-1235 , 
-460 , 
-1232 , 
-465 , 
-1230 , 
-461 , 
-1232 , 
-465 , 
-1232 , 
-1290 , 
-399 , 
-456 , 
-8018 , 
-1292 , 
-385 , 
-1294 , 
-382 , 
-465 , 
-1222 , 
-1295 , 
-388 , 
-1294 , 
-393 , 
-459 , 
-1238 , 
-459 , 
-1235 , 
-456 , 
-1239 , 
-455 , 
-1238 , 
-456 , 
-1234 , 
-1297 , 
-393 , 
-463 , 
-8054 , 
-1295 , 
-384 , 
-1293 , 
-383 , 
-459 , 
-1227 , 
-1294 , 
-385 , 
-1296 , 
-394 , 
-461 , 
-1231 , 
-463 , 
-1232 , 
-462 , 
-1235 , 
-461 , 
-1238 , 
-456 , 
-1232 , 
-1296 , 
-395 , 
-456 , 
-8033 , 
-1291 , 
-386 , 
-1295 , 
-381 , 
-465 , 
-1220 , 
-1298 , 
-384 , 
-1298 , 
-394 , 
-463 , 
-1230 , 
-465 , 
-1230 , 
-461 , 
-1231 , 
-463 , 
-1239 , 
-456 , 
-1231 , 
-1297 , 
-395 , 
-461 , 
-8025 , 
-1293 , 
-385 , 
-1291 , 
-385 , 
-464 , 
-1222 , 
-1296 , 
-387 , 
-1295 , 
-394 , 
-461 , 
-1238 , 
-458 , 
-1234 , 
-460 , 
-1234 , 
-460 , 
-1236 , 
-457 , 
-1236 , 
-1294 , 
-393 , 
-462 };
+	int codes[] = { 1257 , 385 , 1296 , 384 , 457 , 1230 , 1290 , 386 , 1295 , 394 , 462 , 1235 , 460 , 1232 , 465 , 1230 , 461 , 1232 , 465 , 1232 , 1290 , 399 , 456 , 8018 , 1292 , 385 , 1294 , 382 , 465 , 1222 , 1295 , 388 , 1294 , 393 , 459 , 1238 , 459 , 1235 , 456 , 1239 , 455 , 1238 , 456 , 1234 , 1297 , 393 , 463 , 8054 , 1295 , 384 , 1293 , 383 , 459 , 1227 , 1294 , 385 , 1296 , 394 , 461 , 1231 , 463 , 1232 , 462 , 1235 , 461 , 1238 , 456 , 1232 , 1296 , 395 , 456 , 8033 , 1291 , 386 , 1295 , 381 , 465 , 1220 , 1298 , 384 , 1298 , 394 , 463 , 1230 , 465 , 1230 , 461 , 1231 , 463 , 1239 , 456 , 1231 , 1297 , 395 , 461 , 8025 , 1293 , 385 , 1291 , 385 , 464 , 1222 , 1296 , 387 , 1295 , 394 , 461 , 1238 , 458 , 1234 , 460 , 1234 , 460 , 1236 , 457 , 1236 , 1294 , 393 , 462 };
 
 	int result = irSlingRaw(
 		outPin,
@@ -378,151 +139,7 @@ int send_turn(void)
 	double dutyCycle = 0.5;         // The duty cycle of the IR signal. 0.5 means for every cycle,
 	                                // the LED will turn on for half the cycle time, and off the other half
 
-	int codes[] = {
-
-  1283 , 
-385 , 
-1297 , 
-385 , 
-455 , 
-1228 , 
-1292 , 
-390 , 
-1293 , 
-393 , 
-460 , 
-1236 , 
-467 , 
-1222 , 
-1297 , 
-396 , 
-455 , 
-1237 , 
-459 , 
-1236 , 
-462 , 
-1237 , 
-459 , 
-8008 , 
-1292 , 
-385 , 
-1295 , 
-380 , 
-462 , 
-1224 , 
-1297 , 
-384 , 
-1298 , 
-391 , 
-460 , 
-1236 , 
-458 , 
-1232 , 
-1296 , 
-393 , 
-461 , 
-1234 , 
-463 , 
-1231 , 
-463 , 
-1234 , 
-461 , 
-8044 , 
-1299 , 
-380 , 
-1296 , 
-384 , 
-460 , 
-1227 , 
-1292 , 
-388 , 
-1292 , 
-395 , 
-462 , 
-1234 , 
-458 , 
-1233 , 
-1298 , 
-391 , 
-461 , 
-1235 , 
-462 , 
-1233 , 
-463 , 
-1230 , 
-463 , 
-8018 , 
-1296 , 
-384 , 
-1293 , 
-383 , 
-463 , 
-1222 , 
-1299 , 
-382 , 
-1296 , 
-394 , 
-458 , 
-1235 , 
-463 , 
-1229 , 
-1295 , 
-396 , 
-463 , 
-1229 , 
-465 , 
-1229 , 
-463 , 
-1237 , 
-457 , 
-8024 , 
-1294 , 
-381 , 
-1296 , 
-384 , 
-458 , 
-1231 , 
-1291 , 
-389 , 
-1292 , 
-392 , 
-464 , 
-1230 , 
-462 , 
-1234 , 
-1296 , 
-394 , 
-458 , 
-1234 , 
-463 , 
-1233 , 
-461 , 
-1230 , 
-463 , 
-8014 , 
-1291 , 
-387 , 
-1291 , 
-383 , 
-460 , 
-1228 , 
-1291 , 
-390 , 
-1290 , 
-399 , 
-459 , 
-1238 , 
-454 , 
-1238 , 
-1291 , 
-397 , 
-453 , 
-1242 , 
-452 , 
-1242 , 
-454 , 
-1240 , 
-459 };
+	int codes[] = { 1283 , 385 , 1297 , 385 , 455 , 1228 , 1292 , 390 , 1293 , 393 , 460 , 1236 , 467 , 1222 , 1297 , 396 , 455 , 1237 , 459 , 1236 , 462 , 1237 , 459 , 8008 , 1292 , 385 , 1295 , 380 , 462 , 1224 , 1297 , 384 , 1298 , 391 , 460 , 1236 , 458 , 1232 , 1296 , 393 , 461 , 1234 , 463 , 1231 , 463 , 1234 , 461 , 8044 , 1299 , 380 , 1296 , 384 , 460 , 1227 , 1292 , 388 , 1292 , 395 , 462 , 1234 , 458 , 1233 , 1298 , 391 , 461 , 1235 , 462 , 1233 , 463 , 1230 , 463 , 8018 , 1296 , 384 , 1293 , 383 , 463 , 1222 , 1299 , 382 , 1296 , 394 , 458 , 1235 , 463 , 1229 , 1295 , 396 , 463 , 1229 , 465 , 1229 , 463 , 1237 , 457 , 8024 , 1294 , 381 , 1296 , 384 , 458 , 1231 , 1291 , 389 , 1292 , 392 , 464 , 1230 , 462 , 1234 , 1296 , 394 , 458 , 1234 , 463 , 1233 , 461 , 1230 , 463 , 8014 , 1291 , 387 , 1291 , 383 , 460 , 1228 , 1291 , 390 , 1290 , 399 , 459 , 1238 , 454 , 1238 , 1291 , 397 , 453 , 1242 , 452 , 1242 , 454 , 1240 , 459 };
 
 
 	int result = irSlingRaw(
@@ -540,180 +157,12 @@ int send_turn(void)
 
 int send_wave (void)
 {
-    	uint32_t outPin = OUT_PIN;            // The Broadcom pin number the signal will be sent on
+  uint32_t outPin = OUT_PIN;            // The Broadcom pin number the signal will be sent on
 	int frequency = 38000;          // The frequency of the IR signal in Hz
 	double dutyCycle = 0.5;         // The duty cycle of the IR signal. 0.5 means for every cycle,
 	                                // the LED will turn on for half the cycle time, and off the other half
 
-	int codes[] = {
-    1283 , 
-384 , 
-1291 , 
-388 , 
-458 , 
-1227 , 
-1294 , 
-390 , 
-1291 , 
-394 , 
-463 , 
-1235 , 
-459 , 
-1235 , 
-456 , 
-1239 , 
-456 , 
-1236 , 
-1293 , 
-398 , 
-458 , 
-1237 , 
-458 , 
-7985 , 
-1294 , 
-386 , 
-1288 , 
-385 , 
-460 , 
-1227 , 
-1293 , 
-390 , 
-1291 , 
-400 , 
-457 , 
-1233 , 
-460 , 
-1237 , 
-458 , 
-1236 , 
-458 , 
-1237 , 
-1290 , 
-399 , 
-457 , 
-1236 , 
-459 , 
-8021 , 
-1293 , 
-386 , 
-1291 , 
-385 , 
-462 , 
-1225 , 
-1295 , 
-385 , 
-1293 , 
-397 , 
-458 , 
-1237 , 
-457 , 
-1236 , 
-458 , 
-1235 , 
-459 , 
-1238 , 
-1293 , 
-393 , 
-463 , 
-1233 , 
-459 , 
-7993 , 
-1295 , 
-386 , 
-1291 , 
-387 , 
-455 , 
-1228 , 
-1298 , 
-384 , 
-1294 , 
-397 , 
-456 , 
-1239 , 
-455 , 
-1236 , 
-459 , 
-1238 , 
-456 , 
-1232 , 
-1297 , 
-394 , 
-461 , 
-1234 , 
-461 , 
-7995 , 
-1299 , 
-379 , 
-1296 , 
-383 , 
-462 , 
-1223 , 
-1295 , 
-385 , 
-1297 , 
-394 , 
-461 , 
-1233 , 
-483 , 
-1210 , 
-463 , 
-1231 , 
-463 , 
-1231 , 
-1294 , 
-392 , 
-468 , 
-1227 , 
-462 , 
-7982 , 
-1298 , 
-378 , 
-1299 , 
-383 , 
-458 , 
-1229 , 
-1290 , 
-390 , 
-1293 , 
-392 , 
-462 , 
-1235 , 
-459 , 
-1237 , 
-457 , 
-1239 , 
-455 , 
-1233 , 
-1296 , 
-394 , 
-462 , 
-1231 , 
-462 , 
-8018 , 
-1299 , 
-385 , 
-1292 , 
-384 , 
-457 , 
-1228 , 
-1290 , 
-393 , 
-1290 , 
-396 , 
-459 , 
-1238 , 
-455 , 
-1238 , 
-458 , 
-1237 , 
-456 , 
-1236 , 
-1305 , 
-386 , 
-453 , 
-1242 , 
-455 
-   };
+	int codes[] = { 1283 , 384 , 1291 , 388 , 458 , 1227 , 1294 , 390 , 1291 , 394 , 463 , 1235 , 459 , 1235 , 456 , 1239 , 456 , 1236 , 1293 , 398 , 458 , 1237 , 458 , 7985 , 1294 , 386 , 1288 , 385 , 460 , 1227 , 1293 , 390 , 1291 , 400 , 457 , 1233 , 460 , 1237 , 458 , 1236 , 458 , 1237 , 1290 , 399 , 457 , 1236 , 459 , 8021 , 1293 , 386 , 1291 , 385 , 462 , 1225 , 1295 , 385 , 1293 , 397 , 458 , 1237 , 457 , 1236 , 458 , 1235 , 459 , 1238 , 1293 , 393 , 463 , 1233 , 459 , 7993 , 1295 , 386 , 1291 , 387 , 455 , 1228 , 1298 , 384 , 1294 , 397 , 456 , 1239 , 455 , 1236 , 459 , 1238 , 456 , 1232 , 1297 , 394 , 461 , 1234 , 461 , 7995 , 1299 , 379 , 1296 , 383 , 462 , 1223 , 1295 , 385 , 1297 , 394 , 461 , 1233 , 483 , 1210 , 463 , 1231 , 463 , 1231 , 1294 , 392 , 468 , 1227 , 462 , 7982 , 1298 , 378 , 1299 , 383 , 458 , 1229 , 1290 , 390 , 1293 , 392 , 462 , 1235 , 459 , 1237 , 457 , 1239 , 455 , 1233 , 1296 , 394 , 462 , 1231 , 462 , 8018 , 1299 , 385 , 1292 , 384 , 457 , 1228 , 1290 , 393 , 1290 , 396 , 459 , 1238 , 455 , 1238 , 458 , 1237 , 456 , 1236 , 1305 , 386 , 453 , 1242 , 455 };
 
 
 	int result = irSlingRaw(
@@ -726,6 +175,28 @@ int send_wave (void)
 	return result;
 
 }
+
+
+int send_timer(void)
+{
+  uint32_t outPin = OUT_PIN;            // The Broadcom pin number the signal will be sent on
+	int frequency = 38000;          // The frequency of the IR signal in Hz
+	double dutyCycle = 0.5;         // The duty cycle of the IR signal. 0.5 means for every cycle,
+	                                // the LED will turn on for half the cycle time, and off the other half
+
+  int codes[] = {1293 , 381 , 1298 , 386 , 455 , 1232 , 1292 , 389 , 1303 , 394 , 458 , 1241 , 456 , 1235 , 459 , 1239 , 1294 , 395 , 461 , 1236 , 457 , 1237 , 460 , 7986 , 1295 , 387 , 1296 , 383 , 459 , 1233 , 1292 , 391 , 1293 , 397 , 459 , 1240 , 456 , 1237 , 459 , 1238 , 1300 , 397 , 458 , 1237 , 458 , 1240 , 460 , 8018 , 1305 , 391 , 1309 , 387 , 461 , 1230 , 1301 , 391 , 1298 , 399 , 460 , 1239 , 456 , 1242 , 458 , 1235 , 1301 , 400 , 457 , 1240 , 459 , 1243 , 457 , 7994 , 1301 , 387 , 1297 , 389 , 459 , 1225 , 1306 , 391 , 1299 , 397 , 459 , 1241 , 455 , 1238 , 459 , 1239 , 1298 , 396 , 460 , 1234 , 459 , 1240 , 459 , 8003 , 1294 , 391 , 1298 , 387 , 459 , 1228 , 1295 , 394 , 1296 , 396 , 459 , 1241 , 465 , 1228 , 457 , 1240 , 1296 , 400 , 460 , 1235 , 458 , 1241 , 456 , 7994 , 1294 , 391 , 1294 , 390 , 457 , 1228 , 1300 , 391 , 1295 , 401 , 459 , 1237 , 456 , 1241 , 458 , 1233 , 1297 , 401 , 458 , 1238 , 455 , 1241 , 460 };
+
+	int result = irSlingRaw(
+		outPin,
+		frequency,
+		dutyCycle,
+		codes,
+		sizeof(codes) / sizeof(int));
+	
+	return result;
+
+}
+
 
 int on_message(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
   char* payload = message->payload;
@@ -808,64 +279,38 @@ int main (void)
   }
 
   //create device
-  //publish(client, "pac/name", PAC_NAME); 
+  publish(client, "fan/name", FAN_NAME); 
 
   //listen for operation
-  //MQTTClient_subscribe(client, MQTT_TOPIC_TOGGLE_POWER, 0);
-  //MQTTClient_subscribe(client, MQTT_TOPIC_TOGGLE_MODE, 0);
-  //MQTTClient_subscribe(client, MQTT_TOPIC_TOGGLE_FAN, 0);
-  //MQTTClient_subscribe(client, MQTT_TOPIC_TOGGLE_TEMPERATURE, 0);
+  MQTTClient_subscribe(client, MQTT_TOPIC_TOGGLE_POWER, 0);
+  MQTTClient_subscribe(client, MQTT_TOPIC_TOGGLE_WAVE, 0);
+  MQTTClient_subscribe(client, MQTT_TOPIC_TOGGLE_FAN, 0);
+  MQTTClient_subscribe(client, MQTT_TOPIC_TOGGLE_TURN, 0);
+  MQTTClient_subscribe(client, MQTT_TOPIC_TOGGLE_TIMER, 0);
 
   //Default settings
   msg.on = false;
-  msg.temperature = 24;
-  msg.unitF = false;
-  msg.timer = false;
-  msg.timer_value = 1;
-  msg.mode=MODE_BLOW;//MODE_AIRCONDITIONING;
+  msg.turn = false;
+  msg.timer = TIMER_NONE;
+  msg.wave=WAVE_NONE;
   msg.fan=FAN_LOW;
 
-  char temperature[8];
-  char timer_value[8];
-send_power();
-sleep (5);
-send_fan();
-sleep (3);
-send_turn();
-sleep (3);
-send_wave();
-sleep (3);
-send_power();
-return 0;
-
-  send_ir(SIGNAL_POWER); 
-  send_ir(SIGNAL_POWER); 
-  send_ir(SIGNAL_POWER); 
-
-//#define SIGNAL_POWER 	"00100111111100100111111100100111111100100111111100100111111100100111111"
-//#define SIGNAL_FAN    "00100111110100100111110100100111110100100111110100100111110100100111110"
-//#define SIGNAL_TURN	  "00100110111100100110111100100110111100100110111100100110111100100110111"
-//#define SIGNAL_WAVE	  "00100111101100100111101100100111101100100111101100100111101100100111101"
-  return 0;
+  char timer[8];
   
   for (;;) {
     //send temperature measurement
     if (msg.on == true) { publish(client, MQTT_TOPIC_POWER, "on"); } else { publish(client, MQTT_TOPIC_POWER, "off"); }
-    if (msg.unitF == true) { publish(client, MQTT_TOPIC_UNITF, "°F"); } else { publish(client, MQTT_TOPIC_UNITF, "°C"); }
-    if (msg.timer == true) { publish(client, MQTT_TOPIC_TIMER, "on");  } else { publish(client, MQTT_TOPIC_TIMER, "off");  }
-    if (msg.mode == MODE_AIRCONDITIONING) { publish(client, MQTT_TOPIC_MODE, MODE_AIRCONDITIONING_NAME); }
-    else if (msg.mode == MODE_DEHUMIDIFY) { publish(client, MQTT_TOPIC_MODE, MODE_DEHUMIDIFY_NAME); }
-    else if (msg.mode == MODE_BLOW)       { publish(client, MQTT_TOPIC_MODE, MODE_BLOW_NAME); }
+    if (msg.turn == true) { publish(client, MQTT_TOPIC_TIMER, "on");  } else { publish(client, MQTT_TOPIC_TIMER, "off");  }
+    if (msg.wave == WAVE_NONE) { publish(client, MQTT_TOPIC_WAVE, WAVE_MOON_NAME); }
+    else if (msg.wave == WAVE_WOOD) { publish(client, MQTT_TOPIC_WAVE, WAVE_WOOD_NAME); }
+    else if (msg.wave == WAVE_MOON)       { publish(client, MQTT_TOPIC_WAVE, WAVE_NONE_NAME); }
 
     if (msg.fan == FAN_LOW)       { publish(client, MQTT_TOPIC_FAN, FAN_LOW_NAME); }
     else if (msg.fan == FAN_MID)  { publish(client, MQTT_TOPIC_FAN, FAN_MID_NAME);  }
     else if (msg.fan == FAN_HIGH) { publish(client, MQTT_TOPIC_FAN, FAN_HIGH_NAME); }
 
-    sprintf(temperature, "%d", msg.temperature);
-    publish(client, MQTT_TOPIC_TEMPERATURE, temperature); 
-
-    sprintf(timer_value, "%d", msg.timer_value);
-    publish(client, MQTT_TOPIC_TIMER_VALUE, timer_value); 
+    sprintf(timer, "%d", msg.timer);
+    publish(client, MQTT_TOPIC_TIMER, timer); 
 
     sleep(UPDATE_INTERVAL);
   }
